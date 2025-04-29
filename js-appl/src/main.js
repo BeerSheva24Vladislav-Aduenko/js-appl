d09472b92d9a64145d5be0296f4263d4ad6deb66
VladislavAduenko <adyenko.job@gmail.com>
2025-04-29 20:06:26 +0300

    final solution of life game states matrix

--- js-appl/src/main.js
+++ js-appl/src/main.js
@@ -3,4 +3,4 @@ import MatrixComponents from "./components/MatrixComponents.js";
 import configObj from "./config/matrix-config.json";
 import MatrixStates from "./service/MatrixStates.js";
 const { rows, columns, interval } = configObj;
-new MatrixComponents(rows, columns, document.querySelector(".grid"), new MatrixStates());
+new MatrixComponents(rows, columns, document.querySelector(".grid"), new MatrixStates(), interval);
